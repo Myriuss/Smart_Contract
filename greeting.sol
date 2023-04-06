fe1f43e55fc05bbebaa6eaf4e9f67a1ba99e316f
Myriuss <marielelyamani@gmail.com>
2023-04-06 02:48:48 +0000

    :rocket: first try

---
+++ greeting.sol
@@ -0,0 +1,13 @@
+contract Greeting {
+string public name;
+string public greetingPrefix = "Hello ";
+constructor (string memory initialName) {
+name = initialName;
+}
+function setName (string memory newName) public {
+name = newName;
+}
+function getGreeting() public view returns (string memory) {
+return string(abi.encodePacked (greetingPrefix, name));
+}
+}
